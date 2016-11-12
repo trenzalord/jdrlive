68b22bc4c1c3855ae545a17eb76856af56ebd80b
Quentin Gangler <x1q.g1x@gmail.com>
2016-11-12 12:12:04 +0000

    Réimplementation des fonctions live - Lancé de dé (et sauvegarde en bd) - Gestion des personnages (gestion affichage et stats) - Timer - Ajout de la sauvegarde de changement de stats dans le bd (pour les personnages)

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,8 @@
 var socket = io();
 
+var timer = null;
+var elapsed = 0;
+
 /** FUNCTIONS**/
 function userConnect() {
     var loginParams = { login: $('#login_log').val() , password: $('#password_log').val() };
@@ -13,28 +16,108 @@ function userDisconnect() {
 
 function userReconnect() {
     var userId = localStorage.getItem("userId");
-    console.log('Reconnecting with id ' + userId + '...');
     if(userId != null){
-        console.log('userID != null');
         socket.emit('reconnectUser', userId);
     }
+    $('#statsCharacters').html('');
 }
 
 function rollDice(maxRoll, skill) {
-    var diceParams = {max: maxRoll, skill: skill};
+    var characterId = localStorage.getItem("currentCharacter");
+    var saveInBd = document.getElementById("saveInDb").checked;
+    var diceParams = {max: maxRoll, skill: skill, characterId: characterId, saveInBd: saveInBd};
     socket.emit("roll dice", diceParams);
+}
+
+function selectCharacterClick(characterId) {
+    var select = $('#selectCharacter');
+    select.val(characterId);
+    localStorage.setItem("currentCharacter", characterId);
+    var name = select.find('option[value=' + characterId + ']').text();
+    $('#characterPlayed').text("Joue avec : " + name);
+    $('#displayStatsButtons').show();
+}
+
+function addStatsCharacter() {
+    updateDisplayCharacter("add");
+}
+
+function removeStatsCharacter() {
+    updateDisplayCharacter("remove");
+}
+
+function updateLifePoints(type, value) {
+    updateStat("life_point", type, value);
+}
+
+function updateMana(type, value) {
+    updateStat("mana", type, value);
+}
+
+function updateEphirium(type, value) {
+    updateStat("ephirium_tolerance", type, value);
+}
+
+function updateStat(stat, type, value) {
+    console.log("stat: " + stat + ", type: " + type + ", value: " + value);
+    var characterId = localStorage.getItem("currentCharacter");
+    if(characterId != null) {
+        var statParam = {characterId: characterId, stat: stat, type: type, value: value};
+        socket.emit("update character stat", statParam);
+    }
+}
+
+function updateDisplayCharacter(action) {
+    var characterId = localStorage.getItem("currentCharacter");
+    if(characterId != null) {
+        socket.emit(action + " character", characterId);
+    }
 }
 
 function hidePanels() {
     $('.connect-zone').show();
     $('.right-panel').hide();
     $('.left-panel').hide();
+    $('#displayStatsButtons').hide();
+    $('#characterPlayed').text("Joue avec : aucun personnage");
 }
 
 function showPanels() {
     $('.connect-zone').hide();
     $('.right-panel').show();
     $('.left-panel').show();
+}
+
+function createStatsContainer(characterStats) {
+    var html = '<div class="stats" id="stats-' + characterStats.characterId + '" data-id="' + characterStats.characterId + '">' +
+        '<p class="nomStat">' + characterStats.name +'</p>' +
+        '<div class="statBar healthBar"><div></div><span>' + characterStats.life_point + '/' + characterStats.life_point_max + '</span></div>' +
+        '<div class="statBar manaBar"><div></div><span>' + characterStats.mana + '/' + characterStats.mana_max + '</span></div>' +
+        '<div class="statBar ephiriumBar"><div></div><span>' + characterStats.ephirium_tolerance + '/' + characterStats.ephirium_tolerance_max + '</span></div>' +
+        '</div>';
+    $('#statsCharacters').append(html);
+    updateStatsContainer($('#stats-' + characterStats.characterId),
+        characterStats.life_point,
+        characterStats.life_point_max,
+        characterStats.mana,
+        characterStats.mana_max,
+        characterStats.ephirium_tolerance,
+        characterStats.ephirium_tolerance_max);
+}
+
+function updateStatsContainer(container, h, hm, m, mm, e, em){
+    var healthBar = container.find('.healthBar');
+    healthBar.find('span').text(h + '/' + hm);
+    var prcH = (h*100)/hm;
+    healthBar.find('div').css('width', prcH + '%');
+    var manaBar = container.find('.manaBar');
+    manaBar.find('span').text(m + '/' + mm);
+    var prcM = (m*100)/mm;
+    manaBar.find('div').css('width', prcM + '%');
+    var ephiriumBar = container.find('.ephiriumBar');
+    ephiriumBar.find('span').text(e + '/' + em);
+    var prcE = (e*100)/em;
+    ephiriumBar.find('div').css('width', prcE + '%');
 }
 
 function getSkillNameOfSkill(skill) {
@@ -68,11 +151,44 @@ function getSkillNameOfSkill(skill) {
     }
 }
 
+function startTimer(duration) {
+    socket.emit("start timer", duration);
+}
+
+function createTimer(duration) {
+    clearTimer();
+    timer = setInterval(function () {updateTimer(duration)}, 1000);
+}
+
+function updateTimer(duration) {
+    elapsed++;
+    if(elapsed > duration) {
+        clearTimer();
+    } else {
+        updateTimerBar(duration)
+    }
+}
+
+function updateTimerBar(duration) {
+    var value = duration - elapsed;
+    var lvl = value * 100 / duration;
+    $('#barre').css("width", lvl + "%");
+    $('#tempsRestant').text(value + " s");
+}
+
+function clearTimer() {
+    if(timer != null) {
+        clearInterval(timer);
+    }
+    elapsed = 0;
+}
+
 /** SOCKET IO **/
 socket.on('ready', function () {
-    console.log('ready');
+    console.log('Socket.io ready');
     userReconnect();
 });
+
 socket.on("roll dice", function(dice) {
     if(dice.max == dice.value) {
         $('#chiffre').text(dice.value);
@@ -103,7 +219,7 @@ socket.on("roll dice", function(dice) {
 
 socket.on("login", function(userAndCharacters) {
     console.log("Connecté");
-    console.log(userAndCharacters);
+    //console.log(userAndCharacters);
     localStorage.setItem("userId", userAndCharacters.userId);
     var select = $('#selectCharacter');
     select.html('');
@@ -114,10 +230,38 @@ socket.on("login", function(userAndCharacters) {
         }));
     });
     showPanels();
+    var characterId = localStorage.getItem("currentCharacter");
+    if(characterId != null) {
+        selectCharacterClick(characterId);
+    } else {
+        $('#displayStatsButtons').hide();
+    }
 });
 
 socket.on("invalid credentials", function () {
     console.log("Login/Mot de passe incorrect");
+});
+
+socket.on("add character", function (characterStats) {
+    createStatsContainer(characterStats);
+});
+
+socket.on("remove character", function (characterId) {
+    $('#stats-' + characterId).remove();
+});
+
+socket.on("update character stats", function (characterStats) {
+   updateStatsContainer($('#stats-' + characterStats.characterId),
+       characterStats.life_point,
+       characterStats.life_point_max,
+       characterStats.mana,
+       characterStats.mana_max,
+       characterStats.ephirium_tolerance,
+       characterStats.ephirium_tolerance_max);
+});
+
+socket.on("start timer", function (duration) {
+    createTimer(parseInt(duration));
 });
 
 
--- sockets/server.js
+++ sockets/server.js
@@ -1,12 +1,14 @@
 module.exports = function (io, db) {
 
     var bcrypt = require('bcrypt');
+    var random = require('random-js')();
 
     /** Variables **/
     var users = [];
     var characters = [];
 
     var dice = {value: 0, max: 20, characterName: "", skill: "", skillValue: 0};
+    var displayedCharacters = [];
 
     /** Functions **/
     function logMYSQL(msg) {
@@ -18,7 +20,20 @@ module.exports = function (io, db) {
     }
 
     function randomIntInclusive(low, high) {
-        return Math.floor(Math.random() * (high - low + 1) + low);
+        return random.integer(low, high);
+    }
+
+    function testRandom() {
+        var numbers = [];
+        for(var j = 1; j <= 20; j++) {
+            numbers[j-1] = 0;
+        }
+        logSOCKETIO(numbers);
+        for(var i = 0; i < 100000; i++) {
+            var rand = randomIntInclusive(1, 20);
+            numbers[rand-1] += 1;
+        }
+        logSOCKETIO(numbers);
     }
 
     function getAllCharactersOfUser(userId) {
@@ -33,42 +48,73 @@ module.exports = function (io, db) {
         return userCharacters;
     }
 
-    /** Server actions **/
-    db.connect(function (err) {
-        if (err) {
-            logMYSQL('Error connecting: ' + err.stack);
-            return;
-        }
-
-        logMYSQL('Connected');
-    });
-
-    db.query('SELECT * FROM users', function (err, results) {
-        if (err) {
-            logMYSQL('Could not get users: ' + err.stack);
-            return [];
-        }
-        logMYSQL("Retrieved users");
-        results.forEach(function (e) {
-            users[e.id_user] = e;
+    function saveDiceRollInDb(dice, userId) {
+        var critFail = dice.value == 1 ? 1 : 0;
+        var critSuccess = dice.value == dice.max ? 1 : 0;
+        var query = 'UPDATE users SET cpt_dice_roll = cpt_dice_roll + 1, ' +
+            'cpt_critical_success = cpt_critical_success + ?, ' +
+            'cpt_critical_fail = cpt_critical_fail + ? WHERE id_user = ?';
+        db.query(query, [critSuccess, critFail, userId], function(err) {
+            if(err) {
+                logMYSQL('Error saving dice roll:' + err.stack);
+            } else {
+                logMYSQL('Saved dice roll');
+            }
         });
-    });
+    }
 
-    db.query('SELECT * FROM characters', function (err, results) {
-        if (err) {
-            logMYSQL('Could not get characters: ' + err.stack);
-            return [];
-        }
-        logMYSQL("Retrieved characters");
-        results.forEach(function (e) {
-            characters[e.id_character] = e;
+    function saveStatInDb(statName, statChanged, characterId) {
+        characterId = parseInt(characterId);
+        var query = 'UPDATE characters SET ?? = ? WHERE id_character = ?';
+        db.query(query,[statName, statChanged, characterId], function (err) {
+            if(err) {
+                logMYSQL('Error updating stat: ' + err.stack);
+            } else {
+                logMYSQL('Updated stat');
+            }
         });
-    });
+    }
 
+    function getValueOfStat(stat, statMax, statRes, type, value) {
+        type = parseInt(type);
+        switch(type) {
+            case 0:
+                var resultAdd = parseInt(stat) + parseInt(value);
+                if(resultAdd > statMax) {
+                    return statMax;
+                } else {
+                    return resultAdd;
+                }
+            case 1:
+                return stat - value;
+            case 2:
+                var resultSub = 0;
+                if(value > statRes) {
+                    resultSub = value - statRes;
+                }
+                return stat - resultSub;
+            default:
+                return stat;
+        }
+    }
+
+    function getStatResistance(statName) {
+        switch(statName) {
+            case 'life_point':
+                return 'damage_resistance';
+            case 'ephirium_tolerance':
+                return 'ephirium_resistance';
+        }
+    }
+
+    /** SOCKET IO */
     io.on("connection", function(socket) {
         logSOCKETIO("User connected");
         socket.emit('ready');
         socket.emit("roll dice", dice);
+        displayedCharacters.forEach(function(e) {
+           socket.emit('add character', e);
+        });
 
         socket.on("disconnect", function () {
             logSOCKETIO("User disconnected");
@@ -76,12 +122,31 @@ module.exports = function (io, db) {
 
         socket.on("roll dice", function (diceParams) {
             logSOCKETIO("Received roll dice request");
+            var character = characters[diceParams.characterId];
+            var skillValue = 0;
+            var characterName = 'Personne';
+
+            if(character != null) {
+                skillValue = character[diceParams.skill];
+                characterName = character.firstname + ' ' + character.lastname;
+            }
+
+            dice.skillValue = diceParams.skill == "" ? 0 : skillValue != null ? skillValue : 0;
             dice.value = randomIntInclusive(1, diceParams.max);
             dice.max = diceParams.max;
             dice.skill = diceParams.skill;
-            dice.skillValue = diceParams.skill == "" ? 0 : dice.skillValue;
+            dice.characterName = characterName;
+
+            if(diceParams.saveInBd && character != null) {
+                saveDiceRollInDb(dice, character.character_owner_id);
+            }
+
             io.sockets.emit("roll dice", dice);
-            logSOCKETIO("New rolled dice sent (" + dice.value + "/" + dice.max + ")");
+            var savedMsg = diceParams.saveInBd ? ' and saved in database' : '';
+            logSOCKETIO("New rolled dice sent: " + dice.value + "+" + dice.skillValue + "/"
+                + dice.max + ' (' + dice.skill + ") by " + dice.characterName
+                + savedMsg
+            );
         });
 
         socket.on("login", function (loginParams) {
@@ -112,13 +177,103 @@ module.exports = function (io, db) {
 
         socket.on("reconnectUser", function (userId) {
             logSOCKETIO("Received reconnect");
-            users.forEach(function (e) {
-               if(e.id_user == userId) {
-                   logSOCKETIO("User reconnected: " + users[userId].login);
-                   var userAndCharacters = {userId: userId, characters: getAllCharactersOfUser(userId)};
-                   socket.emit("login", userAndCharacters);
-               }
-            });
+            if(users[userId] != null) {
+                logSOCKETIO("User reconnected: " + users[userId].login);
+                var userAndCharacters = {userId: userId, characters: getAllCharactersOfUser(userId)};
+                socket.emit("login", userAndCharacters);
+            }
+        });
+
+        socket.on("add character", function (characterId) {
+            logSOCKETIO("Received add character");
+            if(displayedCharacters[characterId] == null) {
+                var character = characters[characterId];
+                if(character != null) {
+                    var characterStats = {
+                        characterId: characterId,
+                        name: character.firstname + ' ' + character.lastname,
+                        life_point: character.life_point,
+                        life_point_max: character.life_point_max,
+                        mana: character.mana,
+                        mana_max: character.mana_max,
+                        ephirium_tolerance: character.ephirium_tolerance,
+                        ephirium_tolerance_max: character.ephirium_tolerance_max
+                    };
+                    displayedCharacters[characterId] = characterStats;
+                    io.sockets.emit("add character", characterStats);
+                    logSOCKETIO("Added character " + characterStats.name + " to the display");
+                }
+            }
+        });
+
+        socket.on("remove character", function (characterId) {
+            logSOCKETIO("Received remove character");
+            if(displayedCharacters[characterId] != null) {
+                displayedCharacters.splice(characterId, 1);
+                io.sockets.emit("remove character", characterId);
+                logSOCKETIO("Removed character " +
+                    characters[characterId].firstname + " " + characters[characterId].lastname + " from the display");
+            }
+        });
+
+        socket.on("update character stat", function (statParam) {
+            logSOCKETIO("Received character stat update");
+            var character = characters[statParam.characterId];
+            if(character != null) {
+                var statChanged = getValueOfStat(
+                    character[statParam.stat],
+                    character[statParam.stat + '_max'],
+                    character[getStatResistance(statParam.stat)],
+                    statParam.type,
+                    statParam.value);
+                character[statParam.stat] = statChanged;
+                saveStatInDb(statParam.stat, statChanged, statParam.characterId);
+                var displayedCharacter = displayedCharacters[statParam.characterId];
+                if(displayedCharacter != null) {
+                    displayedCharacter[statParam.stat] = statChanged;
+                    io.sockets.emit("update character stats", displayedCharacter);
+                    logSOCKETIO("Updated character stats for " + displayedCharacter.name);
+                }
+            }
+        });
+
+        socket.on("start timer", function (duration) {
+            logSOCKETIO("Received start timer");
+            io.sockets.emit("start timer", duration);
         })
+    });
+
+    /** Server actions **/
+    //testRandom();
+
+    db.connect(function (err) {
+        if (err) {
+            logMYSQL('Error connecting: ' + err.stack);
+            return;
+        }
+
+        logMYSQL('Connected');
+    });
+
+    db.query('SELECT * FROM users', function (err, results) {
+        if (err) {
+            logMYSQL('Could not get users: ' + err.stack);
+            return [];
+        }
+        logMYSQL("Retrieved users");
+        results.forEach(function (e) {
+            users[e.id_user] = e;
+        });
+    });
+
+    db.query('SELECT * FROM characters', function (err, results) {
+        if (err) {
+            logMYSQL('Could not get characters: ' + err.stack);
+            return [];
+        }
+        logMYSQL("Retrieved characters");
+        results.forEach(function (e) {
+            characters[e.id_character] = e;
+        });
     });
 };
